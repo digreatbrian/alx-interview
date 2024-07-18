8ba71761d6e361e06e98d3ce94ee8512cbeb3a86
Brian Musakwa <digreatbrian@gmail.com>
2024-07-18 23:21:57 +0200

    Adding files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-'''
-A script that reads stdin line by line and computes metrics:
 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer, don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-Warning: In this sample, you will have random value - it’s normal to not have the same output as this one.
-'''
 import sys
 
 
@@ -35,20 +22,18 @@ def print_msg(dict_sc, total_file_size):
 total_file_size = 0
 code = 0
 counter = 0
-dict_sc = {
-	"200": 0,
-	"301": 0,
-	"400": 0,
-	"401": 0,
-	"403": 0,
-	"404": 0,
-	"405": 0,
-	"500": 0
-}
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # trimming
+        parsed_line = line.split()  # ✄ trimming
         parsed_line = parsed_line[::-1]  # inverting
 
         if len(parsed_line) > 2:
